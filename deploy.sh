b0e0f3002e6c491986781f1a1a246c32d0ae2f14
Kai Engelhardt <kai@kaiengelhardt.com>
2024-09-01 12:27:48 +0200

    Try different filename

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-put -r _site -hostkey 'ssh-ed25519 255 SHA256:yrt1ZYQO/YULXZ/IXS18adptcEBuDbszghxyfJut/RY'
+put -r ./_site -hostkey 'ssh-ed25519 255 SHA256:yrt1ZYQO/YULXZ/IXS18adptcEBuDbszghxyfJut/RY'
